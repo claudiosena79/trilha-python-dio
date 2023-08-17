11cf3e967105f56784cc442a66809667d393e977
Walter J. Silva <walter4web@gmail.com>
2023-08-17 09:57:20 -0300

    :pencil: docs: add comment in set example

--- 01 - Estrutura de dados/03 - Conjuntos/01_acessando_dados.py
+++ 01 - Estrutura de dados/03 - Conjuntos/01_acessando_dados.py
@@ -1,5 +1,5 @@
-numeros = {1, 2, 3, 2}
+numeros = {1, 2, 3, 2} # declarando o set (conjunto)
 
-numeros = list(numeros)
+numeros = list(numeros) #convertendo para lista
 
-print(numeros[0])
+print(numeros[0]) #acessando a lista (set)
