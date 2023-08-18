0e351c9455434e740240ab044e2184d8af926ab1
Walter J. Silva <walter4web@gmail.com>
2023-08-18 15:18:16 -0300

    :bulb: docs: comment: adicionando valor à lista

--- 01 - Estrutura de dados/04 - Dicionários/00_declarando_dicionarios.py
+++ 01 - Estrutura de dados/04 - Dicionários/00_declarando_dicionarios.py
@@ -4,5 +4,6 @@ print(pessoa)
 pessoa = dict(nome="Guilherme", idade=28)
 print(pessoa)
 
+# Adicionando valor à lista
 pessoa["telefone"] = "3333-1234"  # {"nome": "Guilherme", "idade": 28, "telefone": "3333-1234"}
 print(pessoa)
